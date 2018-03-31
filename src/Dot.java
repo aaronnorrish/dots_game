644f06f74ebc62eab18b21c7a029bb2388992170
aaronnorrish <32919148+aaronnorrish@users.noreply.github.com>
2018-03-31 13:16:54 +0800

    Dot Interface
    Dot interface implemented by the GreenDot and BlueDot classes

--- src/Dot.java
+++ src/Dot.java
@@ -2,6 +2,9 @@ package dots_game;
 
 import java.awt.Graphics;
 
+/**
+ * Dot interface implemented by the GreenDot and BlueDot classes
+ **/
 public interface Dot {
 	public void draw(Graphics g);
 	public int getCentreX();
